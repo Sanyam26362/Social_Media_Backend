0187aee793bbdd7aff00e8e9989f477be23b345d
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-10-28 08:57:28 +0530

    Index.js Created

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/users');
+const app = express();
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+        });
+        console.log('MongoDB connected sucessfully!');     
+    } catch (err) {
+        console.error(err.message);
+
+        process.exit(1);
+    }
+};
+connectDB();
+app.use(express.json());
+app.use('api/auth',authRoutes);
+app.use('api/posts',postRoutes);
+app.use('api/users',userRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log('Server is running on PORT ${PORT}');
+
+});
