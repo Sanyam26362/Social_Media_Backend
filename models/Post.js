d560415a3f72281be0e00dce447ccbe2b88f5dc7
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 20:32:20 +0530

    Error fixed

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,42 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const PostSchema = new Schema({
-    user:{
-        type: Schema.Types.ObjectId,
-        ref:'User',
-        required:true,
-    },
-    content:{
-        type:String,
-        required:true,
-        maxlength:280,
-    },
-    likes:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:'User',
-        },
 
-    ],
-    createdAt:{
-        type: Date,
-        default: Date.now,
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: false,
+    maxlength: 280,
+  },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+ 
+  reposts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+
+ 
+  quotedPost: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+    optional: true,
+  },
 });
-module.exports = mongoose.model('Post',PostSchema);
+
+module.exports = mongoose.model('Post', PostSchema);
