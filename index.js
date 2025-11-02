2a1193d91501384e463864f84c559308bd0d9b5b
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 20:39:58 +0530

    Updated index.js code

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config();
+
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
 const userRoutes = require('./routes/users');
+const commentRoutes = require('./routes/comments');
+const feedRoutes = require('./routes/feed'); 
+
 const app = express();
-const connectDB = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology:true,
-        });
-        console.log('MongoDB connected sucessfully!');     
-    } catch (err) {
-        console.error(err.message);
 
-        process.exit(1);
-    }
-};
 connectDB();
-app.use(express.json());
-app.use('/api/auth',authRoutes);
-app.use('/api/posts',postRoutes);
-app.use('/api/users',userRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>{
-    console.log(`Server is running on PORT ${PORT}`);
 
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/feed', feedRoutes); 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
