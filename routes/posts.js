c862859fb690beeee6eca489d48c6fde4e7e61b0
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 20:19:20 +0530

    Updated the post routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,14 +2,34 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const {
-    createPost,
-    getAllPosts,
-    deletePost,
-    likePost,
-
+  createPost,
+  getAllPosts,
+  deletePost,
+  likePost,
 } = require('../controllers/postController');
-router.post('/',auth,createPost);
-router.get('/',getAllPosts);
-router.delete('/:id',auth,deletePost);
-router.put('/like/:id',auth,likePost);
-module.exports=router;
+
+const {
+  createComment,
+  getCommentsForPost,
+} = require('../controllers/commentController');
+
+
+router.post('/', auth, createPost);
+
+
+router.get('/', getAllPosts);
+
+
+router.delete('/:id', auth, deletePost);
+
+
+router.put('/like/:id', auth, likePost);
+
+
+
+router.post('/:id/comment', auth, createComment);
+
+
+router.get('/:id/comments', getCommentsForPost);
+
+module.exports = router;
