2c2c5fdc907220d4dbeb91292251dfc3faf6cb30
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 12:51:04 +0530

    Fixed the like Post function in postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,92 +1,91 @@
 const Post = require('../models/Post');
-const User = require('../models/User'); 
+const User = require('../models/User');
 
-exports.createPost = async (req, res) => { 
+
+exports.createPost = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
 
     const newPost = new Post({
       content: req.body.content,
       user: req.user.id,
-      username: user.username, 
+      username: user.username,
     });
 
     const post = await newPost.save();
-
-    res.json(post); 
+    res.json(post);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send('Server Error');
   }
 };
 
-exports.getAllPosts = async (req, res) => { 
+
+exports.getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find()
       .populate('user', ['username'])
       .sort({ createdAt: -1 });
-
-    res.json(posts); 
+    res.json(posts);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send('Server Error');
   }
 };
+
 
 exports.deletePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
     if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
+      return res.status(4.04).json({ msg: 'Post not found' });
     }
 
+    
     if (post.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'User not authorized' });
     }
 
     await post.remove();
-    res.json({ msg: 'Post removed' }); 
+    res.json({ msg: 'Post removed' });
   } catch (err) {
     console.error(err.message);
     if (err.kind === 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found' });
     }
-    res.status(500).send('Server error');
+    res.status(500).send('Server Error');
   }
 };
 
+
 exports.likePost = async (req, res) => {
   try {
-   
     const post = await Post.findById(req.params.id);
 
     if (!post) {
       return res.status(404).json({ msg: 'Post not found' });
     }
 
-   
-    
-    if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+    if (post.likes.some((like) => like.toString() === req.user.id)) {
       
-     
       post.likes = post.likes.filter(
-        
-        (like) => like.user.toString() !== req.user.id 
+        (like) => like.toString() !== req.user.id
       );
 
     } else {
       
-     
-      post.likes.unshift({ user: req.user.id });
+      
+      post.likes.unshift(req.user.id);
     }
 
- 
+
     await post.save();
     res.json(post.likes);
 
-  } catch (err) { 
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 };
+
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    userName: {
+    username: {
         type: String,
         required: true,
         unique: true,
