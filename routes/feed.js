b2f17ca96131f078b3151b3554aa8c68aea74469
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 20:37:35 +0530

    Feed routes

---
+++ routes/feed.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const { getFollowingFeed } = require('../controllers/feedController');
+
+=
+router.get('/', auth, getFollowingFeed);
+
+module.exports = router;
