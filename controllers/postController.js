8ae3f3cc11d44765d0099de925e9b425a57b7b22
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-02 20:25:28 +0530

    Updated the post controller to add repost and quote feature

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,27 +4,58 @@ const User = require('../models/User');
 
 exports.createPost = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const { content, quotedPostId } = req.body;
 
-    const newPost = new Post({
-      content: req.body.content,
+    if (!content && !quotedPostId) {
+      return res
+        .status(400)
+        .json({ msg: 'Post content or a quoted post is required' });
+    }
+
+    const postData = {
       user: req.user.id,
-      username: user.username,
-    });
+      content: content,
+    };
 
+    if (quotedPostId) {
+      const postToQuote = await Post.findById(quotedPostId);
+      if (!postToQuote) {
+        return res.status(404).json({ msg: 'Post to quote not found' });
+      }
+      postData.quotedPost = postToQuote.id;
+    }
+
+    const newPost = new Post(postData);
     const post = await newPost.save();
-    res.json(post);
+
+    const populatedPost = await Post.findById(post.id)
+      .populate('user', ['username'])
+      .populate({
+        path: 'quotedPost',
+        populate: {
+          path: 'user',
+          select: 'username',
+        },
+      });
+
+    res.json(populatedPost);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 };
 
-
 exports.getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find()
       .populate('user', ['username'])
+      .populate({
+        path: 'quotedPost',
+        populate: {
+          path: 'user',
+          select: 'username',
+        },
+      })
       .sort({ createdAt: -1 });
     res.json(posts);
   } catch (err) {
@@ -33,20 +64,17 @@ exports.getAllPosts = async (req, res) => {
   }
 };
 
-
 exports.deletePost = async (req, res) => {
   try {
-    
     const post = await Post.findOneAndDelete({
       _id: req.params.id,
-      user: req.user.id, 
+      user: req.user.id,
     });
 
-    
     if (!post) {
-      return res
-        .status(404)
-        .json({ msg: 'Post not found or user not authorized' });
+      return res.status(401).json({
+        msg: 'Post not found or user not authorized',
+      });
     }
 
     res.json({ msg: 'Post removed' });
@@ -59,7 +87,6 @@ exports.deletePost = async (req, res) => {
   }
 };
 
-
 exports.likePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -69,24 +96,41 @@ exports.likePost = async (req, res) => {
     }
 
     if (post.likes.some((like) => like.toString() === req.user.id)) {
-      
       post.likes = post.likes.filter(
         (like) => like.toString() !== req.user.id
       );
-
     } else {
-      
-      
       post.likes.unshift(req.user.id);
     }
 
-
     await post.save();
     res.json(post.likes);
-
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 };
 
+
+exports.repostPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    if (post.reposts.some((repost) => repost.toString() === req.user.id)) {
+      post.reposts = post.reposts.filter(
+        (repost) => repost.toString() !== req.user.id
+      );
+    } else {
+      post.reposts.unshift(req.user.id);
+    }
+
+    await post.save();
+    res.json(post.reposts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
