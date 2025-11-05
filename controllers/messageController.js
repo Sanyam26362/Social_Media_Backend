009c45eea759f9aef0841e21ba2bf495a8bde684
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:54:44 +0530

    message Controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,104 +1,76 @@
+const asyncHandler = require('../middleware/asyncHandler');
+const { body, param } = require('express-validator');
+const validate = require('../middleware/validate');
 const Conversation = require('../models/Conversation');
 const Message = require('../models/Message');
 const User = require('../models/User');
 
+exports.sendMessageValidators = [
+  param('receiverId').isMongoId(),
+  body('content').isString().isLength({ min: 1 }),
+  validate,
+];
 
-exports.sendMessage = async (req, res) => {
-  try {
-    const { content } = req.body;
-    const { receiverId } = req.params;
-    const senderId = req.user.id;
+exports.sendMessage = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const { receiverId } = req.params;
+  const senderId = req.user.id;
 
-    const receiver = await User.findById(receiverId);
-    if (!receiver) {
-      return res.status(404).json({ msg: 'Receiver not found' });
-    }
+  const receiver = await User.findById(receiverId).lean();
+  if (!receiver) return res.status(404).json({ msg: 'Receiver not found' });
 
-    let conversation = await Conversation.findOne({
-      participants: { $all: [senderId, receiverId] },
-    });
+  const conversation = await Conversation.findOneAndUpdate(
+    { participants: { $all: [senderId, receiverId] } },
+    {
+      $setOnInsert: { participants: [senderId, receiverId] },
+      $set: { lastMessageAt: Date.now() },
+    },
+    { upsert: true, new: true }
+  );
 
-    if (!conversation) {
-      conversation = new Conversation({
-        participants: [senderId, receiverId],
-      });
-    }
+  const newMessage = await Message.create({
+    conversation: conversation._id,
+    sender: senderId,
+    receiver: receiverId,
+    content,
+  });
 
-    const newMessage = new Message({
-      conversation: conversation._id,
-      sender: senderId,
-      receiver: receiverId,
-      content: content,
-    });
+  const { io, onlineUsers } = req.app.locals || {};
+  const receiverSocketId = onlineUsers?.get(receiverId);
+  if (io && receiverSocketId) io.to(receiverSocketId).emit('newMessage', newMessage);
 
-    conversation.messages.push(newMessage._id);
-    conversation.lastMessageAt = Date.now();
+  res.status(201).json(newMessage);
+});
 
-    await Promise.all([conversation.save(), newMessage.save()]);
-
-   
-    const { io, onlineUsers } = req.app.locals;
-
-    const receiverSocketId = onlineUsers.get(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit('newMessage', newMessage);
-    }
-
-    res.status(201).json(newMessage);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
-
-
-exports.getConversations = async (req, res) => {
-  try {
-    const conversations = await Conversation.find({
-      participants: req.user.id,
+exports.getConversations = asyncHandler(async (req, res) => {
+  const conversations = await Conversation.find({ participants: req.user.id })
+    .populate('participants', ['username'])
+    .populate({
+      path: 'messages',
+      populate: { path: 'sender', select: 'username' },
+      options: { sort: { createdAt: -1 }, limit: 1 },
     })
-      .populate('participants', ['username'])
-      .populate({
-        path: 'messages',
-        populate: {
-          path: 'sender',
-          select: 'username',
-        },
-        options: { sort: { createdAt: -1 }, limit: 1 } 
-      })
-      .sort({ lastMessageAt: -1 });
-    
-    res.json(conversations);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+    .sort({ lastMessageAt: -1 })
+    .lean();
 
+  res.json(conversations);
+});
 
-exports.getMessages = async (req, res) => {
-  try {
-    const { conversationId } = req.params;
+exports.getMessages = asyncHandler(async (req, res) => {
+  const { conversationId } = req.params;
 
-    const conversation = await Conversation.findOne({
-      _id: conversationId,
-      participants: req.user.id
-    });
+  const conversation = await Conversation.findOne({
+    _id: conversationId,
+    participants: req.user.id,
+  }).lean();
 
-    if (!conversation) {
-      return res.status(403).json({ msg: 'Not authorized to view this conversation' });
-    }
+  if (!conversation) return res.status(403).json({ msg: 'Not authorized to view this conversation' });
 
-    const messages = await Message.find({
-      conversation: conversationId,
-    })
-      .populate('sender', ['username'])
-      .populate('receiver', ['username'])
-      .sort({ createdAt: 1 }); 
+  const messages = await Message.find({ conversation: conversationId })
+    .populate('sender', ['username'])
+    .populate('receiver', ['username'])
+    .sort({ createdAt: 1 })
+    .lean();
 
-    res.json(messages);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+  res.json(messages);
+});
