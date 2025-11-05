920ffce3a603e3147cf0703cbadbbbd26a0e7e13
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:52:50 +0530

    Comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,91 +1,54 @@
-const Post = require('../models/Post');
-const User = require('../models/User');
+const asyncHandler = require('../middleware/asyncHandler');
+const { body, param } = require('express-validator');
+const validate = require('../middleware/validate');
 const Comment = require('../models/Comments');
+const Post = require('../models/Post');
 
+exports.createCommentValidators = [
+  param('id').isMongoId(),
+  body('content').isString().isLength({ min: 1, max: 280 }),
+  validate,
+];
 
-exports.createComment = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
-    }
+exports.createComment = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id).lean();
+  if (!post) return res.status(404).json({ msg: 'Post not found' });
 
-    const newComment = new Comment({
-      content: req.body.content,
-      user: req.user.id,
-      post: req.params.id,
-    });
+  const comment = await Comment.create({
+    content: req.body.content,
+    user: req.user.id,
+    post: req.params.id,
+  });
 
-    const comment = await newComment.save();
+  res.status(201).json(comment);
+});
 
-   
+exports.getCommentsForPost = asyncHandler(async (req, res) => {
+  const comments = await Comment.find({ post: req.params.id })
+    .populate('user', ['username'])
+    .sort({ createdAt: -1 })
+    .lean();
 
-    res.json(comment);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+  res.json(comments);
+});
 
+exports.deleteComment = asyncHandler(async (req, res) => {
+  const comment = await Comment.findById(req.params.id);
+  if (!comment) return res.status(404).json({ msg: 'Comment not found' });
+  if (comment.user.toString() !== req.user.id) return res.status(401).json({ msg: 'User not authorized' });
 
-exports.getCommentsForPost = async (req, res) => {
-  try {
-    const comments = await Comment.find({ post: req.params.id })
-      .populate('user', ['username'])
-      .sort({ createdAt: -1 });
+  await comment.deleteOne();
+  res.json({ msg: 'Comment removed' });
+});
 
-    res.json(comments);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+exports.likeComment = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const comment = await Comment.findById(id);
+  if (!comment) return res.status(404).json({ msg: 'Comment not found' });
 
+  const hasLiked = comment.likes.some((l) => l.toString() === req.user.id);
+  const update = hasLiked ? { $pull: { likes: req.user.id } } : { $addToSet: { likes: req.user.id } };
 
-exports.deleteComment = async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.id);
-
-    if (!comment) {
-      return res.status(404).json({ msg: 'Comment not found' });
-    }
-
-    if (comment.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
-
-    await comment.deleteOne();
-    res.json({ msg: 'Comment removed' });
-  } catch (err)
- {
-    console.error(err.message);
-    if (err.kind === 'ObjectId') {
-      return res.status(404).json({ msg: 'Comment not found' });
-    }
-    res.status(500).send('Server Error');
-  }
-};
-
-
-exports.likeComment = async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.id);
-    if (!comment) {
-      return res.status(404).json({ msg: 'Comment not found' });
-    }
-
-    if (comment.likes.some((like) => like.toString() === req.user.id)) {
-      comment.likes = comment.likes.filter(
-        (like) => like.toString() !== req.user.id
-      );
-    } else {
-      comment.likes.unshift(req.user.id);
-    }
-
-    await comment.save();
-    res.json(comment.likes);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+  const updated = await Comment.findByIdAndUpdate(id, update, { new: true }).select('likes').lean();
+  res.json(updated.likes);
+});
