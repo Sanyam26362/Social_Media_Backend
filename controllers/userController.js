7c52ceed8ec6f37a84f60f4fd9d6012382edfd11
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:56:20 +0530

    Added the necessary imports

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,88 +1,37 @@
+const asyncHandler = require('../middleware/asyncHandler');
 const User = require('../models/User');
 
+exports.getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password').lean();
+  if (!user) return res.status(404).json({ msg: 'User not found' });
+  res.json(user);
+});
 
-exports.getUserProfile = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id).select('-password');
-    if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    if (err.kind === 'ObjectId') {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-    res.status(500).send('Server Error');
-  }
-};
+exports.followUser = asyncHandler(async (req, res) => {
+  if (req.params.id === req.user.id) return res.status(400).json({ msg: 'You cannot follow yourself' });
 
+  const target = await User.findById(req.params.id).lean();
+  if (!target) return res.status(404).json({ msg: 'User not found' });
 
-exports.followUser = async (req, res) => {
-  try {
-    const userToFollow = await User.findById(req.params.id);
-    const currentUser = await User.findById(req.user.id);
+  const me = await User.findByIdAndUpdate(
+    req.user.id,
+    { $addToSet: { following: target._id } },
+    { new: true }
+  );
 
-    if (!userToFollow) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
+  if (!me) return res.status(404).json({ msg: 'Current user not found' });
 
-    if (req.params.id === req.user.id) {
-      return res.status(400).json({ msg: 'You cannot follow yourself' });
-    }
+  await User.findByIdAndUpdate(target._id, { $addToSet: { followers: me._id } });
 
-    if (
-      currentUser.following.some(
-        (follow) => follow.toString() === req.params.id
-      )
-    ) {
-      return res.status(400).json({ msg: 'You are already following this user' });
-    }
+  res.json({ msg: 'User followed' });
+});
 
-    currentUser.following.unshift(userToFollow.id);
-    await currentUser.save();
+exports.unfollowUser = asyncHandler(async (req, res) => {
+  const target = await User.findById(req.params.id).lean();
+  if (!target) return res.status(404).json({ msg: 'User not found' });
 
-    userToFollow.followers.unshift(currentUser.id);
-    await userToFollow.save();
+  await User.findByIdAndUpdate(req.user.id, { $pull: { following: target._id } });
+  await User.findByIdAndUpdate(target._id, { $pull: { followers: req.user.id } });
 
-    res.json({ msg: 'User followed' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
-
-
-exports.unfollowUser = async (req, res) => {
-  try {
-    const userToUnfollow = await User.findById(req.params.id);
-    const currentUser = await User.findById(req.user.id);
-
-    if (!userToUnfollow) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-
-    if (
-      !currentUser.following.some(
-        (follow) => follow.toString() === req.params.id
-      )
-    ) {
-      return res.status(400).json({ msg: 'You are not following this user' });
-    }
-
-    currentUser.following = currentUser.following.filter(
-      (follow) => follow.toString() !== req.params.id
-    );
-    await currentUser.save();
-
-    userToUnfollow.followers = userToUnfollow.followers.filter(
-      (follower) => follower.toString() !== req.user.id
-    );
-    await userToUnfollow.save();
-
-    res.json({ msg: 'User unfollowed' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-};
+  res.json({ msg: 'User unfollowed' });
+});
