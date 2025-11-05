83a5d0e920ce9da037f4a7875ec3119499843899
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:58:11 +0530

    Added the necessary imports

--- routes/messages.js
+++ routes/messages.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
+const { param } = require('express-validator');
 const {
   sendMessage,
   getConversations,
   getMessages,
+  sendMessageValidators,
 } = require('../controllers/messageController');
 
-
-router.post('/:receiverId', auth, sendMessage);
-
-
+router.post('/:receiverId', auth, sendMessageValidators, sendMessage);
 router.get('/conversations', auth, getConversations);
-
-
-router.get('/conversation/:conversationId', auth, getMessages);
+router.get('/conversation/:conversationId', auth, [param('conversationId').isMongoId(), validate], getMessages);
 
 module.exports = router;
