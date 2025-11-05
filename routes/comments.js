c712ab722a6afd6662f0573e90f9c9c8d2f8efdf
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:57:29 +0530

    Added the necessary imports

--- routes/comments.js
+++ routes/comments.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
+const { param } = require('express-validator');
 const {
   deleteComment,
   likeComment,
+  createComment,
+  getCommentsForPost,
+  createCommentValidators,
 } = require('../controllers/commentController');
 
+router.delete('/:id', auth, [param('id').isMongoId(), validate], deleteComment);
+router.put('/like/:id', auth, [param('id').isMongoId(), validate], likeComment);
 
-router.delete('/:id', auth, deleteComment);
-
-
-router.put('/like/:id', auth, likeComment);
+router.post('/:id', auth, createCommentValidators, createComment);
+router.get('/post/:id', [param('id').isMongoId(), validate], getCommentsForPost);
 
 module.exports = router;
