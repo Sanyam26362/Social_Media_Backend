694f146853cc90ca046f2eb17d518e02f0f6fc10
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 11:58:42 +0530

    Added the necessary imports

--- routes/posts.js
+++ routes/posts.js
@@ -1,39 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
+const { param } = require('express-validator');
 
 const {
   createPost,
   getAllPosts,
   deletePost,
   likePost,
-  repostPost, 
+  repostPost,
+  createPostValidators,
 } = require('../controllers/postController');
 
 const {
   createComment,
   getCommentsForPost,
-} = require('../controllers/commentController'); 
+  createCommentValidators,
+} = require('../controllers/commentController');
 
-
-router.post('/', auth, createPost);
-
-
+router.post('/', auth, createPostValidators, createPost);
 router.get('/', getAllPosts);
+router.delete('/:id', auth, [param('id').isMongoId(), validate], deletePost);
+router.put('/like/:id', auth, [param('id').isMongoId(), validate], likePost);
+router.put('/:id/repost', auth, [param('id').isMongoId(), validate], repostPost);
 
-
-router.delete('/:id', auth, deletePost);
-
-
-router.put('/like/:id', auth, likePost);
-
-
-router.put('/:id/repost', auth, repostPost);
-
-
-router.post('/:id/comment', auth, createComment);
-
-
-router.get('/:id/comments', getCommentsForPost);
+router.post('/:id/comment', auth, createCommentValidators, createComment);
+router.get('/:id/comments', [param('id').isMongoId(), validate], getCommentsForPost);
 
 module.exports = router;
