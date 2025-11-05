e9e052981d97b764371be58104792aa7d4e369ff
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-11-05 10:47:34 +0530

    Message Model

---
+++ models/Message.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  conversation: {
+    type: Schema.Types.ObjectId,
+    ref: 'Conversation',
+  },
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('Message', MessageSchema);
